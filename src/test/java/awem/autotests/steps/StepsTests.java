73274ea041774f6401b5f21a9588b8da17544a4c
verengan122 <Crysis00!>
2022-05-10 15:49:14 +0300

    diploma(NR)

--- src/test/java/awem/autotests/steps/StepsTests.java
+++ src/test/java/awem/autotests/steps/StepsTests.java
@@ -5,8 +5,9 @@ import io.qameta.allure.Step;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StepsTests {
     @Step("Смена языка на Англ.")
@@ -22,6 +23,29 @@ public class StepsTests {
     @Step("Проверка успешной смены языка")
     public StepsTests CheckingLanguageChange(){
         $x("//*[contains(text(), 'Create games. Evolve within a team')]").shouldBe(Condition.visible);
+        return this;
+    }
+
+    @Step("Наличие игры по ссылке")
+    public StepsTests thePresenceOfTheGame(){
+        open(baseUrl);
+        $x("//*[@href='https://awem.com/games/']").click();
+        return this;
+    }
+
+    @Step("Проверка наличия игры")
+    public StepsTests CheckingNeedGame(){
+        $x("//*[@class='image-block__content']").shouldBe(Condition.visible);
+        return this;
+    }
+
+    @Step("Проверка заголовка главной страницы AWEM")
+    public StepsTests CheckingTitleAwem(){
+        open(baseUrl);
+            String expectedTitle = "Awem Games";
+            String actualTitle = title();
+
+            assertThat(actualTitle).isEqualTo(expectedTitle);
         return this;
     }
 
