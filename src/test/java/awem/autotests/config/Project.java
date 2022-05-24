38b35081787095f3a5d8ff26bab5226ef7b5c786
verengan122 <Crysis00!>
2022-05-24 03:24:27 +0300

    diploma(NR)

--- src/test/java/awem/autotests/config/Project.java
+++ src/test/java/awem/autotests/config/Project.java
@@ -8,7 +8,8 @@ public class Project {
     public static ProjectConfig config = ConfigFactory.create(ProjectConfig.class, System.getProperties());
 
     public static boolean isWebMobile() {
-        return config.videoStorage() != null || !config.browserMobileView().equals("");
+        Optional<String> optionalS = Optional.ofNullable(config.remoteDriverUrl());
+        return optionalS.isPresent();
     }
 
     public static boolean isRemoteWebDriver() {
