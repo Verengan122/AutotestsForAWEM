10df23f505919ee7cdbb397347e381318ffe2725
verengan122 <Crysis00!>
2022-05-25 14:22:59 +0300

    diploma(NR)

--- src/test/java/awem/autotests/config/Project.java
+++ src/test/java/awem/autotests/config/Project.java
@@ -7,6 +7,11 @@ import java.util.Optional;
 public class Project {
     public static ProjectConfig config = ConfigFactory.create(ProjectConfig.class, System.getProperties());
 
+    public static boolean isWebMobile() {
+        Optional<String> optionalS = Optional.ofNullable(config.remoteDriverUrl());
+        return optionalS.isPresent();
+    }
+
     public static boolean isRemoteWebDriver() {
         Optional<String> optionalS = Optional.ofNullable(config.remoteDriverUrl());
         return optionalS.isPresent();
